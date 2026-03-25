d75073d96ea3fee51a5312ebd19ee9658204e9f9
HamimBhai742 <mdhamim5088@gmail.com>
2026-03-25 16:03:57 +0600

    delete test moidules

--- src/app/modules/test/test.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { testController } from "./test.controller";
-
-const router = Router();
-
-router.get("/m", testController.m);
-
-export const testRoutes = router;
