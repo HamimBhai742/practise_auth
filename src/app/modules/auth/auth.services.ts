f3a03a48d0e0b25906cb5c1ca9e4efb35c6a1c59
HamimBhai742 <mdhamim5088@gmail.com>
2026-03-25 16:00:49 +0600

    update line

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -94,7 +94,9 @@ const verifyOtp = async (email: string, otp: string, data: any) => {
 
 const resetPassword = async (token: string, newPass: string) => {
   const decodedToken = verifyToken(token, env.jwt_secret);
-  console.log(decodedToke);
+  console.log(decodedTok
+
+  );
   const user = await prisma.user.findUnique({ where: { id: decodedToken.id } });
   if (!user) {
     throw new AppError("User not found", httpStatus.NOT_FOUND);
