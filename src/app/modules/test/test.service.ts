f7b05480a77c8cf4652000825636ae665f9419b6
HamimBhai742 <mdhamim5088@gmail.com>
2026-03-25 16:03:11 +0600

    update some line

--- src/app/modules/test/test.service.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const m = async () => {
-  const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return response.data;
-};
-
-export const testService = { m };
